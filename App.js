ea7814914c4a3dd5cc4d7aa80690fd0ddf98779c
Rafael Thomazelli <rafael.thomazelli@gmail.com>
2020-11-19 10:42:57 -0300

    configuracao react navigation

--- App.js
+++ App.js
@@ -13,25 +13,38 @@ import {
 
 import Icon from 'react-native-vector-icons/FontAwesome';
 import { Container, Content, Card, CardItem, Body, Text } from 'native-base';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
 
 Icon.loadFont();
+const Stack = createStackNavigator();
 
-const App: () => React$Node = () => {
+function HomeScreen() {
   return (
     <>
       <StatusBar barStyle="dark-content" />
       <Container>
-          <Content>
-            <Card>
-              <CardItem>
-                <Body>
-                  <Icon name="rocket" size={90} color="#900" />
-                </Body>
-              </CardItem>
-            </Card>
-          </Content>
-        </Container>
+        <Content>
+          <Card>
+            <CardItem>
+              <Body>
+                <Icon name="rocket" size={90} color="#900" />
+              </Body>
+            </CardItem>
+          </Card>
+        </Content>
+      </Container>
     </>
+  );
+}
+
+const App: () => React$Node = () => {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Home" component={HomeScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 };
 
